21c9b65ffe72642b23485f6d966cbcaea87e0099
Sudesh <sudacesrma2058@gmail.com>
2026-03-23 17:16:04 +0545

    FurniVibe Backend

--- Handler/CartLogics.py
+++ Handler/CartLogics.py
@@ -1,7 +1,7 @@
 from django.db import transaction
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view, permission_classes # type: ignore
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from Handler.ApiViewHandler import *
